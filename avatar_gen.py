29cbeb8b364e490d0a8767fa4e6de6ec4a40237e
Suzanne Reed <zanarama17@gmail.com>
2013-07-13 13:22:47 -0400

    Runs independantly of log_generator

--- avatar_gen.py
+++ avatar_gen.py
@@ -20,37 +20,43 @@ import string
 import sys
 import shutil
 import argparse
+import sh
 
 # Create copy of avatar image with Author name as filename
-def create_files(log_path, domains, avatar_dir):
-    with open(log_path) as log:
+def create_files(root_path, domains, avatar_dir):
+    # If dir is a repo then slurp in the formatted log
+    for path, names, files in os.walk(root_path):
+        if '.git' in names:
+            gitpath = os.path.join(path, '.git')
+            contributors = sh.git("--git-dir", gitpath, "--no-pager", "log", "--format='%aN|%aE'").strip().split("\n")
+            # Get the author and their email host
+            for entry in contributors:
+                line_exp = entry.split('|')
+                author = line_exp[0].strip().strip("'")
+                host = line_exp[1].split('@').pop().strip("'")
 
-        # Extract email host and create file path with name
-        # Currently designed to work with a file log_generator doesn't create
-        for entry in log:
-            entry = to_unicode(entry, 'utf-8')
-            line_exp = entry.split('|')
-            avatar = avatar_dir+line_exp[0].rstrip()+".png"
-            host = line_exp[0]
+                avatar = avatar_dir+author+'.png'
+                print(avatar)
+                print(host)
 
-            # Create file
-            if host in domains and not os.path.isfile(avatar):
-                shutil.copyfile(domains[host],avatar)
+                # Create avatar file if it doesn't already exist
+                if host in domains and not os.path.isfile(avatar):
+                    shutil.copyfile(domains[host],avatar)
 
 if __name__ == '__main__':
 
     p = argparse.ArgumentParser(description='Create an avatar file for each select contributors')
-    p.add_argument('avatarFile', required=True, help='A log with all contributors and email host')
-    p.add_argument('domainFile', required=True, help='A file with all domains and cooresponding avatar image')
+    p.add_argument('gitDir', help='The directory of git logs')
+    p.add_argument('domainFile', help='A file with all domains and cooresponding avatar image')
     p.add_argument('-d', '--avatarDir', default=os.getcwd()+'/avatars/', help='Avatar directory')
     args = p.parse_args()
 
-    log_path = args.avatarFile
+    root_path = args.gitDir
     domain_file_location = args.domainFile
     avatar_dir = args.avatarDir
     domains=dict()
 
-    # Read in domain names and the cooresponding avatar
+    # Read in domain names and the corresponding avatar
     with open(domain_file_location) as domain_file:
         for line in domain_file:
             domains[line.split('|')[0].strip()] = line.split('|')[1].strip()
@@ -59,5 +65,5 @@ if __name__ == '__main__':
     if not os.path.exists(avatar_dir):
        os.makedirs(avatar_dir)
 
-    create_files(log_path, domains, avatar_dir)
+    create_files(root_path, domains, avatar_dir)
 
